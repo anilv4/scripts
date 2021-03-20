901a66967e0c67fc8995ccd582004582b57a02c1
Anil Vettathu <anilv4@users.noreply.github.com>
2021-03-20 11:44:01 +0530

    Update snippets.sh

--- snippets.sh
+++ snippets.sh
@@ -12,3 +12,11 @@ for i in `ls ${LOC}/json/*json`; do
   DATE=`echo $i | xargs basename | awk -F"-" '{print $1 "/" $2 "/" $3}'`
   echo "$DATE $VALUES"
 done | uniq > "$LOCPLOT"
+
+LOCATION="/amount/point/file.out"
+while true; do 
+  date 
+  time dd if=/dev/zero of="${LOCATION}.$RANDOM" bs=10K count=50
+  echo "-------" 
+  sleep 1 
+done
